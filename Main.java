5bb0fd4e4ad53f94b4c33c106735ed9bb63b85c4
Hanif <hanif.hakim@spesolution.com>
2022-11-26 13:32:50 +0700

    First Program Java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    /*
+     
+        Run javac to create Bin File
+        Run Bin file to run program with java <filename>
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
